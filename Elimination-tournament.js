008870d0ac04afeb8caceef58cbd377f3e33adc3
Jey <126204426+Jeyrama@users.noreply.github.com>
2024-06-10 16:32:54 -0700

    Update Elimination-tournament.js

--- Elimination-tournament.js
+++ Elimination-tournament.js
@@ -30,3 +30,15 @@ Notes:
 
 
 // Solution
+
+const tourney = arr => {
+  if (arr.length === 1)
+    return [ arr ];
+  
+  let next = arr.length % 2 ? arr.slice(-1) : [];
+
+  for (let i = 0; i < arr.length - 1; i += 2)
+    next.push(Math.max(arr[i], arr[i + 1]));
+  
+  return [ arr ].concat(tourney(next));
+}
